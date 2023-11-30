e278dfeba62640ad36711fde3875977192db6369
Bijen Hirachan <bijen.hirachan@gmail.com>
2023-11-30 05:51:03 +0100

    Favicon and logo updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/recipemanager.png";
 
 const Header = () => {
   return (
     <header className="h-[10svh] bg-indigo-600 flex items-center justify-between px-4 md:px-16 py-8">
       <Link className="text-indigo-100 text-xl font-bold" to={"/"}>
-        Logo
+        <img className="h-14" src={logo} alt="" />
       </Link>
       <ul className="flex gap-4">
         <li className="text-indigo-50 font-semibold text-sm">
